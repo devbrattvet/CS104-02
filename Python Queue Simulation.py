19289a8347d3f51ded13431a60e9be7748d4cd01
devbrattvet <71794506+devbrattvet@users.noreply.github.com>
2020-11-25 15:21:56 -0500

    Queue Simulation

---
+++ Python Queue Simulation.py
@@ -0,0 +1,8 @@
+names = []
+numberofnames = 0
+for numberofnames in range (10):
+     acceptedName = input("Please enter a name: ")
+     names.append(acceptedName)
+     numberofnames = numberofnames + 1
+while len(names) > 0:
+     print(names.pop(0))
